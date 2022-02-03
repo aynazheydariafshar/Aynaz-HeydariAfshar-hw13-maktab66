2808477b79df38d7693bc79aff32066ff84d7e19
Aynaz HeydariAfshar <aynazhadriafshar@gmail.com>
2022-02-03 11:56:57 +0330

    add clone & delete func

--- ex1/ex1.js
+++ ex1/ex1.js
@@ -1,29 +1,87 @@
+//add row at first table
 $(document).ready(function(){
-    $("#btn-add").click(function(e){
-        e.preventDefault();
-        let count = $(".item").length + 1;
-        $(".first").before(''+
-        '<div class="item">'+
-            '<tr class="first-' + count + '">'+
-                '<td scope="row">'+
-                    '<button type="button" class="btn btn-danger"><i class="bi bi-trash-fill mx-1"></i>Delete</button>'+
-                    '<button type="button" class="btn btn-primary"><i class="bi bi-sticky-fill mx-1"></i>clone</button>'+
-                '</td>'+
-                '<td>'+
-                    '<input type="number" class="form-control" id="inputNumber" name="number">'+
-                '</td>'+
-                '<td>'+
-                    '<input type="text" class="form-control" id="inputText" name="text">'+
-                '</td>'+
-                '<td>'+
-                    '<input class="form-check-input" type="radio" name="status" id="typeNew">'+
-                    '<label class="form-check-label me-4" for="typeNew">New</label>'+
-                    '<input class="form-check-input" type="radio" name="status" id="typeInprogess">'+
-                    '<label class="form-check-label me-4" for="typeInprogess">In progess</label>'+
-                    '<input class="form-check-input" type="radio" name="status" id="typeConfirmed">'+
-                    '<label class="form-check-label me-4" for="typeConfirmed">Confirmed</label>'+
-                '</td>'+
-            '</tr>'+
-        '</div>')
+    let count =  1;
+    $("#btn-add").click(function(){
+        count++;
+        $("tbody").prepend(
+        '<tr>'+
+            '<td scope="row">'+
+                '<button type="button" class="btn btn-danger deleted mx-1"><i class="bi bi-trash-fill mx-1"></i>Delete</button>'+
+                '<button type="button" class="btn btn-primary clone mx-1"><i class="bi bi-sticky-fill mx-1"></i>clone</button>'+
+            '</td>'+
+            '<td>'+
+                '<input type="number" class="form-control input-check" id="inputNumber" name="number">'+
+            '</td>'+
+            '<td>'+
+                '<input type="text" class="form-control input-check" id="inputText" name="text">'+
+            '</td>'+
+            '<td>'+
+                '<input class="form-check-input" value="0" type="radio" name="status-' + count + '" id="typeNew">'+
+                '<label class="form-check-label me-4 label-new" for="typeNew">New</label>'+
+                '<input class="form-check-input" value="1" type="radio" name="status -' + count + '" id="typeInprogess">'+
+                '<label class="form-check-label me-4 label-new" for="typeInprogess">In progess</label>'+
+                '<input class="form-check-input" value="2" type="radio" name="status -' + count + '" id="typeConfirmed">'+
+                '<label class="form-check-label me-4 label-confirmed" for="typeConfirmed">Confirmed</label>'+
+            '</td>'+
+        '</tr>');
+        updateRow(1);
+        notConfirmed();
     });
-});
+
+
+    $(document).on('click', ".deleted", function () {
+        $(this).closest('tr').remove();
+        updateRow(-1);
+        notConfirmed();
+    });
+
+
+    $(document).on('click', ".clone", function (e) {
+        let rowToClone = $(this).closest('tr').clone();
+        let typeRadioes = $(rowToClone).find("input[type=radio]");
+        count++;
+        for (let item of typeRadioes)
+        {
+            $(item).attr('name', 'group-' + count);
+        }
+        $(this).closest('tr').after(rowToClone);
+        updateRow(1);
+        notConfirmed();
+    });
+
+
+    let counter = 1
+    function updateRow(number) {
+        counter = counter + number
+        $("#numAll").html(counter)
+    }
+
+
+    $(document).on('click', '.label-confirmed', function () {
+        $(this).parents('tr').find('.input-check').attr('disabled', true);
+        notConfirmed();
+    });
+    $(document).on('click', '.label-new', function () {
+        $(this).parents('tr').find('.input-check').removeAttr('disabled');
+        notConfirmed();
+    });
+
+
+     let countconfirmed=1;
+     $("#nymNotConfrimed").html(countconfirmed);
+
+    function notConfirmed () {
+        const rowTable = $("tbody tr");
+        countconfirmed = 0;
+        for (let item of rowTable)
+        {
+            let typeConfrimed = $(item).find("input[type=radio][value=2]").is(":checked");
+            if (!typeConfrimed)
+            {
+                countconfirmed++;
+            }
+        }
+        $("#nymNotConfrimed").html(countconfirmed)
+    }
+}); 
+
