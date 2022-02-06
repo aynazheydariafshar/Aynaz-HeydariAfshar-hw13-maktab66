0b9f22db0ec93bd58a10097f411e63bea69cf3f0
Aynaz HeydariAfshar <aynazhadriafshar@gmail.com>
2022-02-06 15:49:47 +0330

    change time

--- ex2/ex2.js
+++ ex2/ex2.js
@@ -1,9 +1,9 @@
 $("#accordion > li > div").click(function(){
 
 	if(false == $(this).next().is(':visible')) {
-		$('#accordion ul').slideUp(300);
+		$('#accordion ul').slideUp(700);
 	}
-	$(this).next().slideToggle(300);
+	$(this).next().slideToggle(700);
 });
 
 $('#accordion ul:eq(0)').show();
